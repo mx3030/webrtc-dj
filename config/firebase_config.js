5732666b7ce31226dda35b83385a2e22af753eb6
mx3030 <mxpayne3030@gmail.com>
2024-06-09 16:05:07 +0200

    fast

--- config/firebase_config.js
+++ config/firebase_config.js
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
     apiKey: "AIzaSyCmP157u6f0uHAA2Hk-IqUsZN4gPBPdi4g",
     authDomain: "webrtc-dj-1dd44.firebaseapp.com",
+    databaseURL: "https://webrtc-dj-1dd44-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "webrtc-dj-1dd44",
     storageBucket: "webrtc-dj-1dd44.appspot.com",
     messagingSenderId: "118950096357",
